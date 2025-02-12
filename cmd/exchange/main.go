4ffc2fc1fbaf12b6cb8576550558d57fe648c183
Reza Mokaram <rezamokaramdost@gmail.com>
2025-02-12 23:30:37 +0330

    feat(redis)
    - redis added to compose

--- cmd/exchange/main.go
+++ cmd/exchange/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 
-	"github.com/RezaMokaram/ExchangeService/api/server"
 	"github.com/RezaMokaram/ExchangeService/config"
-	gorm "github.com/RezaMokaram/ExchangeService/pkg/postgres"
 
 	"github.com/RezaMokaram/ExchangeService/api/handlers/http"
 	"github.com/RezaMokaram/ExchangeService/app"
@@ -18,27 +15,10 @@ func main() {
 	flag.StringVar(&path, "cleanenv", "./config/config.json", "path to clean env config file")
 	flag.Parse()
 
-	cfg, err := config.LoadConfig(path)
-	if err != nil {
-		log.Fatalf("load config failed: %v\n", err.Error())
-	}
+	c := config.MustReadConfig(path)
+	// c := config.AConfig{}
 
-	db, err := gorm.NewGormDatabase(&cfg.Postgres)
-	if err != nil {
-		log.Fatalf("db connection failed: %v\n", err.Error())
-	}
+	appContainer := app.NewMustApp(c)
 
-	fmt.Println("Database operations done.")
-
-	go func() {
-		c := config.MustReadConfig(path)
-		// c := config.AConfig{}
-
-		appContainer := app.NewMustApp(c)
-
-		log.Fatal(http.Run(appContainer, c.Server))
-	}()
-
-	e := server.NewServer()
-	server.RunServer(e, db, &cfg.App)
+	log.Fatal(http.Run(appContainer, c.Server))
 }
